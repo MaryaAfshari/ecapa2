8b4dbc118af6775f8da9bd9d57ba82d9b7496a97
afshari-maryam <afshari1431@gmail.com>
2023-11-27 10:06:38 +0200

    just write the EER

--- files/trainECAPAModel.py
+++ files/trainECAPAModel.py
@@ -98,15 +98,20 @@ score_file = open(args.score_save_path, "a+")
 while(1):
 ## Training for one epoch
     #loss, lr, acc = s.train_network(epoch = epoch, loader = trainLoader)
-    loss, lr, acc = s.train_net(epoch = epoch, loader = trainLoader)
+    #loss, lr, acc = s.train_net(epoch = epoch, loader = trainLoader)
 
 ## Evaluation every [test_step] epochs
     if epoch % args.test_step == 0:
         s.save_parameters(args.model_save_path + "/model_%04d.model"%epoch)
         EERs.append(s.eval_network(eval_list = args.eval_list, eval_path = args.eval_path)[0])
+        print("----------------------------------------------------------------------")
+        print(time.strftime("%Y-%m-%d %H:%M:%S"), "%d epoch, EER %2.2f%%, bestEER %2.2f%%"%(epoch,  EERs[-1], min(EERs)))
+        score_file.write("%d epoch, EER %2.2f%%, bestEER %2.2f%%\n"%(epoch,  EERs[-1], min(EERs)))
+        score_file.flush()
+        '''print("----------------------------------------------------------------------")
         print(time.strftime("%Y-%m-%d %H:%M:%S"), "%d epoch, ACC %2.2f%%, EER %2.2f%%, bestEER %2.2f%%"%(epoch, acc, EERs[-1], min(EERs)))
         score_file.write("%d epoch, LR %f, LOSS %f, ACC %2.2f%%, EER %2.2f%%, bestEER %2.2f%%\n"%(epoch, lr, loss, acc, EERs[-1], min(EERs)))
-        score_file.flush()
+        score_file.flush()'''
 
     if epoch >= args.max_epoch:
         quit()
